d718abddac46fe290fc068cbfd4449cfcfec6a6f
Joto Chelidze <38420759+Jotochelidze@users.noreply.github.com>
2018-05-26 16:13:42 +0200

    Created a Movie Class. Added RecyclerView

--- app/src/main/java/com/jotoc/moviesdb/MainActivity.java
+++ app/src/main/java/com/jotoc/moviesdb/MainActivity.java
@@ -2,12 +2,19 @@ package com.jotoc.moviesdb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Make a reference to RecyclerView
+        recyclerView = findViewById(R.id.recyclerView);
+
     }
 }
