cc04651a3d2a49e9542dc38de37d79d64ac9ec0b
Joto Chelidze <38420759+Jotochelidze@users.noreply.github.com>
2018-06-05 17:13:11 +0200

    Implemented butterknife. Fixed bugs.

--- app/src/main/java/com/jotoc/moviesdb/DetailActivity.java
+++ app/src/main/java/com/jotoc/moviesdb/DetailActivity.java
@@ -2,55 +2,59 @@ package com.jotoc.moviesdb;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CollapsingToolbarLayout;
+
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
 
-    TextView nameOfMovie, plotSynopsis, userRating, releaseDate;
+    @BindView(R.id.moviePoster)
     ImageView imageView;
+    @BindView(R.id.movietitle)
+    TextView nameOfMovie;
+    @BindView(R.id.plotsynopsis)
+    TextView plotSynopsis;
+    @BindView(R.id.userrating)
+    TextView userRating;
+    @BindView(R.id.release_date)
+    TextView releaseDate;
 
     @Override
-    public void onCreate (Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-        imageView = findViewById(R.id.moviePoster);
-        nameOfMovie = findViewById(R.id.title);
-        plotSynopsis = findViewById(R.id.plotsynopsis);
-        userRating = findViewById(R.id.userrating);
-        releaseDate = findViewById(R.id.release_date);
-
-
         Intent intentThatStartedThisActivity = getIntent();
-        if(intentThatStartedThisActivity.hasExtra("original_title")){
+        if (intentThatStartedThisActivity.hasExtra("original_title")) {
 
-            String thumbnail = getIntent().getExtras().getString("poster_path");
+            String picture = getIntent().getExtras().getString("poster_path");
             String movieName = getIntent().getExtras().getString("original_title");
             String synopsis = getIntent().getExtras().getString("overview");
             String rating = getIntent().getExtras().getString("vote_average");
             String dateOfRelease = getIntent().getExtras().getString("release_date");
 
             Glide.with(this)
-                    .load(thumbnail)
+                    .load(picture)
                     .into(imageView);
-
             nameOfMovie.setText(movieName);
             plotSynopsis.setText(synopsis);
             userRating.setText(rating);
             releaseDate.setText(dateOfRelease);
-        }else{
+
+
+        } else {
             Toast.makeText(this, "No_API_KEY", Toast.LENGTH_SHORT).show();
         }
     }
